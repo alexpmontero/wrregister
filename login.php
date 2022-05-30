299dd0c4f87047116707b3a1577ba1a7c70dd88b
Alex Perez Montero <aperez@redcompanies.com.mx>
2022-05-30 15:22:25 -0500

    estilos a validaciones

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
- 
 include 'config.php';
 session_start();
  
@@ -28,7 +27,6 @@ if (isset($_POST['login'])) {
         }
     }
 }
- 
 ?>
 
 <head>
@@ -88,4 +86,34 @@ if (isset($_POST['login'])) {
       </div>
     </div>
   </form>
+    <?php
+      include 'config.php';
+      session_start();
+      
+      if (isset($_POST['login'])) {
+      
+          $username = $_POST['username'];
+          $password = $_POST['password'];
+      
+          $query = $connection->prepare("SELECT * FROM usuarios WHERE USERNAME=:username");
+          $query->bindParam("username", $username, PDO::PARAM_STR);
+          $query->execute();
+      
+          $result = $query->fetch(PDO::FETCH_ASSOC);
+      
+          if (!$result) {
+              echo '<p class="error">*Username password combination is wrong!</p>';
+          } else {
+              if (password_verify($password, $result['password'])) {
+                  $_SESSION['user_id'] = $result['usuario_id'];
+                  $_SESSION['user_name'] = $result['username'];
+                  //echo '<p class="success">Congratulations, you are logged in!</p>';
+                  header('Location: admin');
+                  exit;
+              } else {
+                  echo '<p class="error">**Username password combination is wrong!</p>';
+              }
+          }
+      }
+    ?>
 </section>
